f38043d5b59b529ebfc3d5172b1cf81fd1d506e3
Ildar Khabatulin <psyhister@gmail.com>
2015-03-11 23:03:59 +0600

    Lil' fix
    It's hard being an idiot

--- algo/karatsuba_mul/karatsuba_mul.cpp
+++ algo/karatsuba_mul/karatsuba_mul.cpp
@@ -26,17 +26,22 @@ int karatsuba_mul(const int x, const int y) {
 	} while(second > 0);
 
 	const size_t max_digits2 = std::max(first_digits, second_digits) / 2;
-	int a = x / power_ten(first_digits / 2), b = x - a * power_ten(first_digits / 2),
-	c = y / power_ten(second_digits / 2), d = y - c * power_ten(second_digits / 2);
-
+	int a = x / power_ten(max_digits2), b = x - a * power_ten(max_digits2),
+	c = y / power_ten(max_digits2), d = y - c * power_ten(max_digits2);
+	std::cout << a << " " << b << " " << c << " " << d << std::endl;
 	int ac = karatsuba_mul(a, c), bd = karatsuba_mul(b, d),
 	    part_sum = karatsuba_mul(a + b, c + d) - ac - bd;
 	return ac * power_ten(max_digits2 * 2) +
 		part_sum * power_ten(max_digits2) + bd;
 }
 
-int main() {
-	std::cout << karatsuba_mul(-1020, 30430) << std::endl;
-	std::cout << -1020 * 30430 << std::endl;
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		std::cerr << "2 arguments required" << std::endl;
+		return 1;
+	}
+	int x = atoi(argv[1]), y = atoi(argv[2]);
+	std::cout << "Karatsuba result: " << karatsuba_mul(x, y) << std::endl;
+	std::cout << "Normal multiplication result: " << x * y << std::endl;
 	return 0;
 }
